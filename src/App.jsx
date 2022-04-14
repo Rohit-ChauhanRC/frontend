2cc3b4b72be4e5ca859b71c01a5533f325fdb9a1
ROHIT CHAUHAN <rc672800@gmail.com>
2022-04-14 11:15:04 +0530

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Navigation from "./components/shared/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <h1>hhhh</h1>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
