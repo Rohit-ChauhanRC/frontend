c7e4f62dfc439059b092ee993a20b4f6e46a9300
Rohit-ChauhanRC <rc672800@gmail.com>
2022-04-15 14:11:14 +0530

    register and login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navigation from "./components/shared/Navigation/Navigation";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Navigation />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
         </Routes>
       </Router>
     </div>
