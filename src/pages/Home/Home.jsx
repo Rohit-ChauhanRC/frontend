b70f52fd1ca9a26bbd0c70d7985190f9eef9255d
ROHIT CHAUHAN <rc672800@gmail.com>
2022-04-25 17:51:44 +0530

    1:38

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,37 +5,35 @@ import Card from "../../components/shared/card/Card";
 import Button from "../../components/shared/Button/Button";
 
 const Home = () => {
-
   const signINLink = {
-    color: '#0077ff',
-    fontWeight: 'bold',
+    color: "#0077ff",
+    fontWeight: "bold",
     textDecoration: "none",
-    marginLeft:'20px',
-  }
+    marginLeft: "20px",
+  };
 
   const navigate = useNavigate();
 
   function startRegister() {
-    navigate('/register');
+    navigate("/authenticate");
   }
   return (
     <div className={styles.cardWrapper}>
-      <Card title={"Welcome to Codershouse!"} icon={"/images/hand.png"} >
-
-      <p className={styles.text}>
+      <Card title={"Welcome to Codershouse!"} icon={"/images/hand.png"}>
+        <p className={styles.text}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quas
           optio eos odit praesentium, ea vero magnam ducimus et culpa.
         </p>
         <div>
-          <Button onClick={startRegister} text="Get your username" />
+          <Button onClick={startRegister} text="Let's go" />
         </div>
         <div className={styles.signinWrapper}>
           <span className={styles.hasInvite}>Have an invite text?</span>
-          <Link style={signINLink} to="/login">Sign in</Link>
+          {/* <Link style={signINLink} to="/login">
+            Sign in
+          </Link> */}
         </div>
-
       </Card>
-
     </div>
   );
 };
